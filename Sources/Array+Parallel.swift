40477ed72983fbf2a991b36066e5764cf8380e72
cbepxbeo <cbepxbeo@mail.ru>
2023-04-02 22:27:05 +0300

    add Array+Parallel

---
+++ Sources/Array+Parallel.swift
@@ -0,0 +1,16 @@
+/*
+ 
+ Project: ParallelSequence
+ File: Array+Parallel.swift
+ Created by: Egor Boyko
+ Date: 02.04.2023
+ 
+ Status: #In progress | #Not decorated
+ 
+*/
+
+import Foundation
+
+extension Array {
+    final public class Parallel<Element> {}
+}
