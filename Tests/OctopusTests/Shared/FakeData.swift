84e77680d9142a23fc4689faee3516cd5f5d42a3
cbepxbeo <cbepxbeo@mail.ru>
2023-04-03 22:29:16 +0300

    add FakeData

---
+++ Tests/OctopusTests/Shared/FakeData.swift
@@ -0,0 +1,15 @@
+/*
+ 
+ Project: Octopus
+ File: FakeData.swift
+ Created by: Egor Boyko
+ Date: 03.04.2023
+ 
+ 
+*/
+
+struct FakeData {
+    let int: Int
+    let string: String
+    let bool: Bool
+}
